846e9b9f885701a38cfbdf18992d46e1887a0494
Massimo Valle <maxvalle12@gmail.com>
2020-03-20 17:17:13 +0100

    update player class - general fix

--- src/main/java/it/polimi/ingsw/Model/Player.java
+++ src/main/java/it/polimi/ingsw/Model/Player.java
@@ -1,5 +1,6 @@
 package it.polimi.ingsw.Model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Player {
@@ -7,34 +8,48 @@ public class Player {
     private String playerName;
     private God playerGod;
     private List<Worker> playerWorkers;
+    private final int numWorker = 2;
 
     private boolean moved;
     private boolean built;
 
-    public Player(String playerName, God playerGod) {
+    public Player(String playerName) {
         this.playerName = playerName;
-        this.playerGod = playerGod;
         this.moved = false;
         this.built = false;
+
+        this.playerWorkers = new ArrayList<>();
+        for (int i = 0; i < numWorker; i++) { this.playerWorkers.add(new Worker()); }   // 2 workers
     }
 
 
-    public boolean hasMoved() { return true; }
-    public boolean hasBuilt() { return true; }
+    public boolean hasMoved() { return moved; }
+    public boolean hasBuilt() { return built; }
 
-    public void choseGod() { };
+
+    public void choseGodsFromDeck() { };
+
+
+    public void assignGodToPlayer(God selectedGod) { this.playerGod = selectedGod; }
+
 
     public void placeWorker() { };
 
-    public void choseWorkerToUse() { };
 
-    public void assignGod() { };
+    public void choseWorkerToUse(Worker selectedWorker) {
+        Worker currentWorker = this.playerWorkers.get(this.playerWorkers.indexOf(selectedWorker));
+
+        // do something
+    };
+
+
+
 
 
     @Override
     public String toString() {
-        return "Nome Giocatore :" + this.playerName +
-                "\nDivinita :" + this.playerGod;
+        return "Player Name: " + this.playerName.toUpperCase() +
+                "\nGod: " + this.playerGod.getGodName().toUpperCase();
     }
 }
 
