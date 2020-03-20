e2d280dabfe7d01bf5af7be05e47e6a5bea68409
Massimo Valle <maxvalle12@gmail.com>
2020-03-20 15:02:29 +0100

    Converted the deck class to singleton
    Converted the deck class to singleton and implemented some methods

--- src/main/java/it/polimi/ingsw/Model/Deck.java
+++ src/main/java/it/polimi/ingsw/Model/Deck.java
@@ -1,14 +1,31 @@
 package it.polimi.ingsw.Model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Deck {
 
+    private static Deck istance=null;
     private List<God> gods;
-    
-    public void initDeck() { };
+    private final int numGods = 5;
 
-    public void removeCard() { };
+    private Deck() {
+        gods = new ArrayList<>();
+        for (int i = 0; i < numGods; i++) { gods.add(new God()); }
+    }
+
+    public static Deck getIstance() {
+        if(istance==null)
+            istance = new Deck();
+        return istance;
+    }
+
+
+    public void removeGod(God selectedGod) {
+        gods.remove(selectedGod);
+    }
+
+    // public void initDeck() { };  // -> default constructor is the new initDeck()
 
 
 }
