ce6394153a0e33c5b44c5accdf0e5a5c3e8f945e
Massimo Valle <maxvalle12@gmail.com>
2020-04-17 15:53:08 +0200

    changed message management

--- src/main/java/it/polimi/ingsw/Network/Message/Message.java
+++ src/main/java/it/polimi/ingsw/Network/Message/Message.java
@@ -2,27 +2,39 @@ package it.polimi.ingsw.Network.Message;
 
 import java.io.Serializable;
 
-public abstract class Message implements Serializable {
+public class Message implements Serializable {
     private String messageSender;
     private MessageContent messageContent;
+    private MessageStatus messageStatus;
+    private String message;
 
 
-    public Message(String messageSender, MessageContent messageContent) {
+    public Message(String messageSender, String message) {
         this.messageSender = messageSender;
+        this.message = message;
+    }
+
+    public Message(String messageSender, MessageContent messageContent, String message) {
+        this(messageSender, message);
         this.messageContent = messageContent;
     }
 
-    public Message(MessageContent messageContent) {
-        this.messageContent = messageContent;
+    public Message(String messageSender, MessageStatus messageStatus, String message) {
+        this(messageSender, message);
+        this.messageStatus = messageStatus;
     }
 
-    public String getMessageSender() {
-        return messageSender;
-    }
-
-    public MessageContent getMessageContent() {
-        return messageContent;
+    public Message(String messageSender, MessageContent messageContent, MessageStatus messageStatus, String message) {
+        this(messageSender, messageContent, message);
+        this.messageStatus = messageStatus;
     }
 
 
+    public String getMessageSender() { return messageSender; }
+
+    public MessageContent getMessageContent() { return messageContent; }
+
+    public MessageStatus getMessageStatus() { return messageStatus; }
+
+    public String getMessage() { return message; }
 }
