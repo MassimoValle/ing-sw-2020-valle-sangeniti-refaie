a280b2b6ed84823dc6949ebae9a31e63574dbbc4
Simone Sangeniti <snakenextgen@gmail.com>
2020-05-05 09:59:53 +0200

    Game singleton && gameState unique in SuperMegaController
    -The Game.getInstance is used in order to create a ModelSerialized instance that will be sent to every player

---
+++ src/main/java/it/polimi/ingsw/Model/ModelSerialized.java
@@ -0,0 +1,23 @@
+package it.polimi.ingsw.Model;
+
+import it.polimi.ingsw.Model.Map.GameMap;
+import it.polimi.ingsw.Model.Player.Player;
+
+import java.io.Serializable;
+
+/**
+ * This is the model that will be sent every time a request is performed by any client to update their respective remoteView
+ *
+ *
+ */
+public class SerializedModel implements Serializable {
+
+    public GameMap gameMap;
+
+
+
+    public SerializedModel
+
+
+
+}
--- src/main/java/it/polimi/ingsw/Model/SerializedModel.java
+++
@@ -1,9 +0,0 @@
-package it.polimi.ingsw.Model;
-
-import java.io.Serializable;
-
-public class SerializedModel extends Game implements Serializable {
-
-
-
-}
