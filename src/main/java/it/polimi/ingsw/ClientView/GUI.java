b28aeaac628e629e5acc71736eeb13cea70ae866
Massimo Valle <maxvalle12@gmail.com>
2020-05-12 14:33:00 +0200

    GUI: mainView added

---
+++ src/main/java/it/polimi/ingsw/ClientView/GUI.java
@@ -0,0 +1,34 @@
+package it.polimi.ingsw.ClientView;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+
+
+public class GUI extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        Parent root = FXMLLoader.load(getClass().getResource("mainView.fxml"));
+
+        Scene scene = new Scene(root);
+
+        stage.setTitle("GUI");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    /*public GUI(){
+
+    }*/
+}
